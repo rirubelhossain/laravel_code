0607fe3b6efa1e30c9307a7c901dd263f07649ca
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-06-10 14:56:06 -0700

    Add files via upload

--- posts.blade.php
+++ posts.blade.php
@@ -5,29 +5,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
 </head>
 <body>
 
-    <h1>All post </h1>
-    <?php echo "this data come from database ";?>
-    @foreach($posts  as $variable)
-        <h3>{{$variable->title}}</h3>
-        <?php
-        echo "this is title variable = ". $variable->title ;
-        ?>
-        <p>{{$variable->body}}</p>
-    @endforeach
-
-    <?php
     
-    foreach($posts as $var )
-    {
-        echo "Inside the php = ". $var->title ." And " . $var->body ;
-    }
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            All Posts
+
+                        </div>
+                            <div class="card-body">
+                                <table class = "table">
+
+                                    <thread>
+                                        <tr>
+                                            <th>Post Title</th>
+                                            <th>Post Body</th>
+                                        </tr>
+                                    </thread>
+
+                                    <tbody>
+                                        @foreach($posts as $post)
+                                        <tr>
+                                            <td>{{$post->title}}</td>
+                                            <td>{{$post->body}}</td>
+                                        </tr>
+                                        @endforeach        
+                                    </tbody>
+                                </table>
+                            </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </section>
+
+
     
-    ?>
-
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </body>
 </html>
 
